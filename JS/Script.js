1dd12cab725f70d75e6f625279fa6011ad13ce78
Heitor Oliveira de França <heitoroliveira8@hotmail.com>
2020-04-26 18:14:50 -0300

    Tela cadastro concluida com JS funcionando

---
+++ JS/Script.js
@@ -0,0 +1,8 @@
+function cadProx(){
+    document.getElementById("telacad1").style.visibility = "hidden";
+    document.getElementById("telacad2").style.visibility = "visible";
+};
+function cadVolt(){
+    document.getElementById("telacad2").style.visibility = "hidden";
+    document.getElementById("telacad1").style.visibility = "visible";
+};
